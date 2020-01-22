b9d2b282fda8b79d715398219b18d69b58477d21
Samir Dahal <samirdahal175@gmail.com>
2020-01-22 11:59:16 +0545

    updated

--- js/animate.js
+++ js/animate.js
@@ -43,3 +43,17 @@ courses.forEach(course => {
 //footer copy right dynamic date
 $('.copyright').html(`&copy; ${new Date().getFullYear()} All Rights Reserved.`);
 $('.designed-by').html('Designed and developed by <a class="samir" href="https://fb.com/lahadrimas" target="_blank">Samir Dahal</a>');
+
+//check toggling navbar
+$('.navbar-toggler').on('click', (e) => {
+    if (!$('#menu').hasClass('show')) {
+        $('.overlay').addClass('active');
+    }
+    else {
+        $('.overlay').removeClass('active');
+    }
+})
+$('.overlay').on('click', (e) => {
+    $('#menu').removeClass('show');
+    $('.overlay').removeClass('active');
+})
