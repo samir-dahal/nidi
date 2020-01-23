e0bbbee388f7644881f937aa0f43295748aeed85
Samir Dahal <samirdahal175@gmail.com>
2020-01-23 15:09:29 +0545

    updated navlink

--- js/animate.js
+++ js/animate.js
@@ -13,7 +13,9 @@ function animateCSS(element, animationName, callback) {
 //smooth scroll
 $(document).ready(function () {
     $("a").on('click', function (event) {
-
+        if ($('#menu').hasClass('show')) {
+            $('#menu').removeClass('show');
+        }
         if (this.hash !== "") {
             event.preventDefault();
 
